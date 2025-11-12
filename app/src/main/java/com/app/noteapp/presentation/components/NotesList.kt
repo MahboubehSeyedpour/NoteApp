9e493bcf3f9c971b7ebdc5478ba507b87c71cb6a
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2025-11-12 22:36:53 +0330

    chore(ui): change the color of divider for pinned note

--- app/src/main/java/com/app/noteapp/presentation/components/NotesList.kt
+++ app/src/main/java/com/app/noteapp/presentation/components/NotesList.kt
@@ -55,7 +55,7 @@ fun NotesList(
             HorizontalDivider(
                 modifier = Modifier.padding(vertical = dimensionResource(R.dimen.screen_padding)),
                 thickness = 1.dp,
-                color = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.35f)
+                color = MaterialTheme.colorScheme.primary.copy(alpha = 0.38f)
             )
         }
 
